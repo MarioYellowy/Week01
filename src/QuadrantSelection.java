153ccfa6ca47781b37ae5461dd0c55b7e23ef474
Mario <mario03eche@gmail.com>
2024-07-05 12:49:38 -0600

    Repositorio creado

---
+++ src/QuadrantSelection.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class QuadrantSelection {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
