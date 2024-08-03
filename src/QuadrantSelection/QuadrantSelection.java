d7be5baae523ab8f0e1fbf1caec471a197b0b260
Mario <mario03eche@gmail.com>
2024-08-03 14:47:24 -0600

    fix: implementations fix

--- src/QuadrantSelection/QuadrantSelection.java
+++ src/QuadrantSelection/QuadrantSelection.java
@@ -3,19 +3,20 @@ package QuadrantSelection;
 import java.util.Scanner;
 
 public class QuadrantSelection {
-    public static void main(String[] args) {
+
+    public static void getQuadrant() {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         int y = sc.nextInt();
 
-        if (-1000<x && x<1000 && x!=0 && -1000<y && y<1000 && y!=0) {
-            if (x<0 && y>0 ) {
+        if (-1000 < x && x < 1000 && x != 0 && -1000 < y && y < 1000 && y != 0) {
+            if (x < 0 && y > 0) {
                 new showResult(1);
-            } else if (x>0 && y>0) {
+            } else if (x > 0 && y > 0) {
                 new showResult(2);
-            } else if (x<0 && y<0) {
+            } else if (x < 0 && y < 0) {
                 new showResult(3);
-            } else if (x>0 && y<0) {
+            } else if (x > 0 && y < 0) {
                 new showResult(4);
             }
         } else {
